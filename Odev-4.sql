8c1f051329598a3c465256d98cd53d37c5bfa7ad
lutfubaycol <lutfu_mrcool@hotmail.com>
2026-01-16 20:02:34 +0300

    odev 7

--- Odev-4.sql
+++ Odev-4.sql
@@ -8,12 +8,15 @@ SELECT COUNT (DISTINCT replacement_cost) FROM film ;
 
 -- 3. film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 
-SELECT COUNT (*) FROM film WHERE title LIKE 'T%' AND rating = 'G'
+SELECT COUNT (*) FROM film 
+WHERE title LIKE 'T%' AND rating = 'G'
 
 -- 4. country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 
-SELECT COUNT(*) FROM country WHERE LENGTH(country) = 5;
+SELECT COUNT(*) FROM country 
+WHERE LENGTH(country) = 5;
 
 -- 5. city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 
-SELECT COUNT (*) FROM city WHERE city ILIKE '%r' ;
+SELECT COUNT (*) FROM city 
+WHERE city ILIKE '%r' ;
